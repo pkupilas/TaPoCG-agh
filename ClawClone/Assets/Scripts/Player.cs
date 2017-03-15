6dbd8831264c2961a32ec1ae3e7220f4a715a8bb
pkupilas <pm.kupilas@gmail.com>
2017-03-15 11:24:02 +0100

    #2 Improved player movement.

--- ClawClone/Assets/Scripts/Player.cs
+++ ClawClone/Assets/Scripts/Player.cs
@@ -7,36 +7,33 @@ public class Player : MonoBehaviour
 {
 
     private Vector2 _move;
-    private float _move_power = 5;
+    private float _move_acceleration = 10f;
+    private float _jump_acceleration = 300f;
     private Rigidbody2D _rigidbody;
 
+
 	// Use this for initialization
-	void Start ()
+	private void Start ()
 	{
 	    _rigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	private void Update ()
 	{
 	    var h = CrossPlatformInputManager.GetAxis("Horizontal");
 	    var v = CrossPlatformInputManager.GetAxis("Vertical");
 
         _move = (Vector2.right*h).normalized;
-        _rigidbody.AddForce(_move * _move_power);
-        var jump = CrossPlatformInputManager.GetButton("Space");
-        Debug.Log("Colliding.");
-        if (jump)
-        {
-            _rigidbody.AddForce(new Vector2(0, 1) * 20);
-        }
-        //Debug.Log("Horizontal: " + h + "Vertical: " + v + "Move: " + _move + "Space: " + jump);
+        _rigidbody.AddForce(_move * _move_acceleration, ForceMode2D.Force);
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionStay2D(Collision2D other)
     {
-
-
+        var jump = CrossPlatformInputManager.GetButtonDown("Space");
+        if (jump && other.gameObject.CompareTag("Ground"))
+        {
+            _rigidbody.AddForce(Vector2.up * _jump_acceleration, ForceMode2D.Force);
+        }
     }
-
 }
