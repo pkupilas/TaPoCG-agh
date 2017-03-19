2cea30797071113041bb3964e4b7b211d53fbf85
pkupilas <pm.kupilas@gmail.com>
2017-03-19 21:33:18 +0100

    Added comments.

--- ClawClone/Assets/Scripts/Player.cs
+++ ClawClone/Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
 
     public Transform groundChecker;
 
-    private Vector2 _targetDistance;
+    private Vector2 _targetDistance;                    // way to go by player
     private float _move_acceleration = 15f;
     private float _jump_acceleration = 6f;
     private Rigidbody2D _rigidbody;
@@ -24,11 +24,13 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        //Check if player is staying on ground
         _grounded = Physics2D.Linecast(transform.position, groundChecker.position, 1 << LayerMask.NameToLayer("Ground"));
         _targetDistance = Vector2.zero;
         _horizontalMoveInput = CrossPlatformInputManager.GetAxis("Horizontal");
         _isJumping = CrossPlatformInputManager.GetButtonDown("Space");
 
+        //init way to go
         _targetDistance = Vector2.right * Time.deltaTime * _move_acceleration * _horizontalMoveInput;
 
         if (_targetDistance != Vector2.zero)
@@ -37,6 +39,7 @@ public class Player : MonoBehaviour
             _rigidbody.velocity += _targetDistance;
         }
 
+        //check if player is on ground and if space has been pressed
         if (_grounded && _isJumping)
         {
             _grounded = false;
@@ -44,6 +47,7 @@ public class Player : MonoBehaviour
         }
     }
 
+    //rotate player to the moving direction
     private void RotateIfChangeDirection()
     {
         transform.eulerAngles = _targetDistance.x < 0 ? new Vector2(0, 0) : new Vector2(0, 180);
