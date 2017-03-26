9db4d8e3e6b877f8a920e90c0945278722619b2c
pkupilas <pm.kupilas@gmail.com>
2017-03-26 20:40:04 +0200

    #13 Added treasure prefabs.

---
+++ ClawClone/Assets/Scripts/Treasure.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasure : Item
+{
+
+    [SerializeField] private int points;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        base.OnTriggerEnter2D(other);
+        Debug.Log("+" + points + " Points!");
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.GetComponent<Player>()!=null)
+        {
+            base.OnCollisionEnter2D(other);
+        }
+        Debug.Log("Collision entered.");
+    }
+}
