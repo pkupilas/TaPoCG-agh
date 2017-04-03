6543126c7365f30a21473d49154592b855e20521
pkupilas <pm.kupilas@gmail.com>
2017-04-03 20:19:47 +0200

    #31 Added spawning random item and destroying box gameobject after DestroyBox animation.

---
+++ ClawClone/Assets/Scripts/Box.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+
+    public Item[] spawningItems;
+
+    // Used in BoxDestroy animation as animation event 
+    private void SpawnRandomItem()
+    {
+        if (spawningItems.Length>0)
+        {
+            int randomIndex = Random.Range(0, spawningItems.Length);
+            var boxPosition = transform.position;
+            Instantiate(spawningItems[randomIndex], boxPosition, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogWarning("SpawningItems array is not initialized in inspector.");
+        }
+    }
+
+    // Used in BoxDestroy animation as animation event
+    private void DestroyBox()
+    {
+        Destroy(gameObject);
+    }
+}
