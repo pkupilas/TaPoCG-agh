176ae9b2b2b6dfda5612ed906414dfcf5456a0ec
aalices <aa.warchal@gmail.com>
2017-04-04 21:57:42 +0200

    Add ExtraSkill potions

---
+++ ClawClone/Assets/Scripts/ExtraSkill.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraSkill : Item {
+
+    public enum Skill { DoubleJump, Run};
+
+    [SerializeField]
+    private Skill _skill;
+
+    protected override void OnCollisionEnter2D(Collision2D other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            base.OnCollisionEnter2D(other);
+            player.changeCurrentSkill(_skill);
+        }
+    }
+}
