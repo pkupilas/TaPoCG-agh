aa6bc14927ba8ef681f7f483c61851f441f1ff25
pkupilas <pm.kupilas@gmail.com>
2017-04-06 21:05:05 +0200

    #7 Added Enemy1 prefab. #12 Added movement animation. #8 Added partial move handling.

---
+++ ClawClone/Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+
+    [SerializeField] private float _move_acceleration = 4f;
+    [SerializeField] private float _distance = 3f;
+    private float _lastIdlePosition;
+    private Vector2 _direction = Vector2.right;
+
+    // Use this for initialization
+    void Start ()
+	{
+	    _animator = GetComponent<Animator>();
+	    _rigidbody = GetComponent<Rigidbody2D>();
+	    _lastIdlePosition = transform.position.x;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    CheckIfMove();
+	}
+
+    private void CheckIfMove()
+    {
+
+        if (transform.position.x-(_lastIdlePosition+_distance)>Mathf.Epsilon)
+        {
+            _animator.SetBool("isWalking", false);
+            _lastIdlePosition = transform.position.x;
+            transform.eulerAngles = new Vector2(0, 180);
+            _rigidbody.velocity = Vector2.zero;
+            _direction = Vector2.left;
+        }
+        else
+        {
+            _animator.SetBool("isWalking", true);
+            _rigidbody.velocity += _direction * Time.deltaTime * _move_acceleration;
+        }
+        //if (_animator.GetBool("isWalking"))
+        //{
+        //    _rigidbody.velocity += _direction * Time.deltaTime * _move_acceleration;
+        //}
+    }
+}
