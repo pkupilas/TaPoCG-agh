97415a64df55ecf1d76fd68d59bafee2d4e82abc
pkupilas <pm.kupilas@gmail.com>
2017-04-10 21:05:01 +0200

    #8 Improved enemy movement. #10 Added and set idle animation.

--- ClawClone/Assets/Scripts/Enemy.cs
+++ ClawClone/Assets/Scripts/Enemy.cs
@@ -8,8 +8,8 @@ public class Enemy : MonoBehaviour
     private Animator _animator;
     private Rigidbody2D _rigidbody;
 
-    [SerializeField] private float _move_acceleration = 4f;
-    [SerializeField] private float _distance = 3f;
+    [SerializeField] private float _moveAcceleration = 50f;
+    [SerializeField] private float _distance = 4f;                  // Value how far enemy can walk
     private float _lastIdlePosition;
     private Vector2 _direction = Vector2.right;
 
@@ -19,34 +19,46 @@ public class Enemy : MonoBehaviour
 	    _animator = GetComponent<Animator>();
 	    _rigidbody = GetComponent<Rigidbody2D>();
 	    _lastIdlePosition = transform.position.x;
-
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	    CheckIfMove();
+	    Move();
 	}
 
-    private void CheckIfMove()
+    private void Move()
     {
-
-        if (transform.position.x-(_lastIdlePosition+_distance)>Mathf.Epsilon)
+        // Change direction of moving after distance
+        if (Mathf.Abs(transform.position.x - _lastIdlePosition) > _distance)
         {
             _animator.SetBool("isWalking", false);
             _lastIdlePosition = transform.position.x;
-            transform.eulerAngles = new Vector2(0, 180);
             _rigidbody.velocity = Vector2.zero;
-            _direction = Vector2.left;
+            RotateEnemy();
+            ChangeDirectionVector();
         }
-        else
+
+        // Set velocity when walking animation is enabled
+        if(_animator.GetBool("isWalking"))
         {
-            _animator.SetBool("isWalking", true);
-            _rigidbody.velocity += _direction * Time.deltaTime * _move_acceleration;
+            _rigidbody.velocity = _direction * Time.deltaTime * _moveAcceleration;
         }
-        //if (_animator.GetBool("isWalking"))
-        //{
-        //    _rigidbody.velocity += _direction * Time.deltaTime * _move_acceleration;
-        //}
+    }
+
+    // Used in EnemyIdle animation as animation event 
+    private void SetIsWalkingBool()
+    {
+        _animator.SetBool("isWalking", true);
+    }
+
+    private void RotateEnemy()
+    {
+        transform.eulerAngles = _direction.x < 0 ? new Vector2(0, 0) : new Vector2(0, 180);
+    }
+
+    private void ChangeDirectionVector()
+    {
+        _direction = _direction == Vector2.left ? Vector2.right : Vector2.left;
     }
 }
