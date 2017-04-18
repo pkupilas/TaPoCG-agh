02df733908cc80f56ef90e2581b84dd29df1ddb0
pkupilas <pm.kupilas@gmail.com>
2017-04-18 16:23:34 +0200

    Added enemy attack handling.

--- ClawClone/Assets/Scripts/Enemy.cs
+++ ClawClone/Assets/Scripts/Enemy.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
+    private bool _isPlayerSpottedOnBack = false;
+    private bool _isPlayerSpottedOnFront = false;
     private Animator _animator;
     private Rigidbody2D _rigidbody;
 
+    [SerializeField] private GameObject[] _spottingPoints;
+    [SerializeField] private float _damage = -10f;
     [SerializeField] private float _moveAcceleration = 50f;
     [SerializeField] private float _distance = 4f;                  // Value how far enemy can walk
     private float _lastIdlePosition;
@@ -23,9 +26,53 @@ public class Enemy : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update ()
-	{
-	    Move();
+    {
+        Debug.DrawLine(transform.position, _spottingPoints[0].transform.position, Color.red);
+        Debug.DrawLine(transform.position, _spottingPoints[1].transform.position, Color.red);
+        /*SPOTTING PLAYER:
+         * - chceck distance between sprites
+         * - use fancy stuff -> Linecast checking
+         */
+        LookForPlayer();
+        
+        if (_isPlayerSpottedOnBack || _isPlayerSpottedOnFront)
+        {
+            if (_isPlayerSpottedOnBack)
+            {
+                Turn();
+            }
+
+	        Attack();
+	    }
+	    else
+	    {
+            Move();
+        }
 	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var player = other.gameObject.GetComponent<Player>();
+            //player.TakeDamage(_damage);
+        }
+    }
+
+    private void LookForPlayer()
+    {
+        _isPlayerSpottedOnBack = Physics2D.Linecast(transform.position, _spottingPoints[0].transform.position,
+            1 << LayerMask.NameToLayer("Player"));
+
+        _isPlayerSpottedOnFront = Physics2D.Linecast(transform.position, _spottingPoints[1].transform.position,
+            1 << LayerMask.NameToLayer("Player"));
+    }
+
+    private void Attack()
+    {
+        _animator.SetBool("isWalking", false);
+        _animator.SetBool("isAttacking", true);
+    }
 
     private void Move()
     {
@@ -35,8 +82,7 @@ public class Enemy : MonoBehaviour
             _animator.SetBool("isWalking", false);
             _lastIdlePosition = transform.position.x;
             _rigidbody.velocity = Vector2.zero;
-            RotateEnemy();
-            ChangeDirectionVector();
+            Turn();
         }
 
         // Set velocity when walking animation is enabled
@@ -50,6 +96,18 @@ public class Enemy : MonoBehaviour
     private void SetIsWalkingBool()
     {
         _animator.SetBool("isWalking", true);
+    }
+
+    // Used in EnemyAttack animation as animation event 
+    private void UnsetIsAttackingBool()
+    {
+        _animator.SetBool("isAttacking", false);
+    }
+
+    private void Turn()
+    {
+        RotateEnemy();
+        ChangeDirectionVector();
     }
 
     private void RotateEnemy()
