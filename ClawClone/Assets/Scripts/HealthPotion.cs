38347e5a5b717b1124a4faffdcf30a7cbc16b6db
aalices <aa.warchal@gmail.com>
2017-04-18 15:42:33 +0200

    Add missing files

---
+++ ClawClone/Assets/Scripts/HealthPotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotion : Item {
+
+    public enum Skill { DoubleJump, Run, None };
+
+    [SerializeField]
+    private int hp;
+
+    protected override void OnCollisionEnter2D(Collision2D other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            base.OnCollisionEnter2D(other);
+            player.TakeDamage(hp);
+        }
+    }
+}
