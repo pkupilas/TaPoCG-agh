ec726f049a9b0d03ebc2d8431db32cf1b995f1fa
aalices <aa.warchal@gmail.com>
2017-05-06 13:17:02 +0200

    Add ladder prefab and interacting

---
+++ ClawClone/Assets/Scripts/Ladder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour {
+
+    [SerializeField]
+    private BoxCollider2D platform;
+
+	public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Player>().onLadder = true;
+            platform.enabled = false;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Player>().onLadder = false;
+            platform.enabled = true;
+        }
+    }
+
+}
--- ClawClone/Assets/Scripts/Player.cs
+++ ClawClone/Assets/Scripts/Player.cs
@@ -8,11 +8,15 @@ public class Player : MonoBehaviour
 {
     public Transform groundChecker;
 
+    public bool onLadder;
+
     private Vector2 _targetDistance;                    // way to go by player
     private float _moveAcceleration = 15f;
     private float _jumpAcceleration = 6f;
+    private float _climbAcceleration = 2f;
     private Rigidbody2D _rigidbody;
     private float _horizontalMoveInput;
+    private float _verticalMoveInput;
     private bool _jumpPressed;
     private bool _canRejump = false;
     private bool _grounded;
@@ -47,6 +51,7 @@ public class Player : MonoBehaviour
         _grounded = Physics2D.Linecast(transform.position, groundChecker.position, 1 << LayerMask.NameToLayer("Ground"));
         _targetDistance = Vector2.zero;
         _horizontalMoveInput = CrossPlatformInputManager.GetAxis("Horizontal");
+        _verticalMoveInput = CrossPlatformInputManager.GetAxis("Vertical");
         _jumpPressed = CrossPlatformInputManager.GetButtonDown("Space");
 
         //init way to go
@@ -59,20 +64,34 @@ public class Player : MonoBehaviour
         }
 
         // if arrow pressed - walk animation
-        if (_horizontalMoveInput != 0)
+        if (!onLadder)
         {
-            _anim.SetBool("isWalking", true);
-            _anim.SetFloat("inputX", CrossPlatformInputManager.GetAxisRaw("Horizontal"));
+            _rigidbody.gravityScale = 1;
+            if (_horizontalMoveInput != 0)
+            {
+                _anim.SetBool("isWalking", true);
+                _anim.SetFloat("inputX", CrossPlatformInputManager.GetAxisRaw("Horizontal"));
+            }
+            else
+            {
+                _anim.SetBool("isWalking", false);
+            }
+
         }
         else
         {
-            _anim.SetBool("isWalking", false);
+            _rigidbody.gravityScale = 0;
+            if (_verticalMoveInput != 0)
+            {
+                _rigidbody.velocity = new Vector2(0, _climbAcceleration * _verticalMoveInput);
+            }
         }
-
+        
         //check if player is on ground and if space has been pressed
         if (_grounded)
         {
             _anim.SetBool("isFalling", false);
+            _anim.SetBool("isJumping", false);
             if (_jumpPressed)
             {
                 _grounded = false;
@@ -96,6 +115,9 @@ public class Player : MonoBehaviour
             else if (_rigidbody.velocity.y < 0)
             {
                 _anim.SetBool("isFalling", true);
+            } else
+            {
+                _anim.SetBool("isFalling", false);
             }
 
         }
