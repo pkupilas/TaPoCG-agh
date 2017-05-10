a35a60f327b34468c8dc7687c09e0739491c6af9
pkupilas <pm.kupilas@gmail.com>
2017-05-10 10:08:53 +0200

    Changed player movement. Updated prefab.

---
+++ ClawClone/Assets/Scripts/Utilities/StandardValues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Utilities
+{
+    static class StandardValues
+    {
+        // Player
+        public const float PlayerMoveAcceleration = 5f;
+        public const float PlayerJumpAcceleration = 6f;
+        public const float PlayerClimbAcceleration = 2f;
+        public const float PlayerMaxHealth = 100f;
+    }
+}
