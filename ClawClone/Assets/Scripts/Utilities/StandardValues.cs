650c74b0edb32c688d3f0a22bcfc7ad74d1858b5
pkupilas <pm.kupilas@gmail.com>
2017-05-11 21:49:30 +0200

    Adjusted PlayerMoveAcceleration.

--- ClawClone/Assets/Scripts/Utilities/StandardValues.cs
+++ ClawClone/Assets/Scripts/Utilities/StandardValues.cs
@@ -8,7 +8,7 @@ namespace Assets.Scripts.Utilities
     static class StandardValues
     {
         // Player
-        public const float PlayerMoveAcceleration = 5f;
+        public const float PlayerMoveAcceleration = 4f;
         public const float PlayerJumpAcceleration = 6f;
         public const float PlayerClimbAcceleration = 2f;
         public const float PlayerMaxHealth = 100f;
