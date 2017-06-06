f776ceb5c6f9907a3200e97ea03818b6d464363e
aalices <aa.warchal@gmail.com>
2017-06-06 09:46:15 +0200

    Enemy spawns an item after death

---
+++ ClawClone/Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public Item[] spawningItems;
+
+    void SpawnRandomItem()
+    {
+        if (spawningItems.Length > 0)
+        {
+            int randomIndex = Random.Range(0, spawningItems.Length);
+            var boxPosition = transform.position;
+            Instantiate(spawningItems[randomIndex], boxPosition, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogWarning("SpawningItems array is not initialized in inspector.");
+        }
+    }
+}
+
