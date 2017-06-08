391ff21706143554e91f580e65bbded6f16789dc
pkupilas <pm.kupilas@gmail.com>
2017-06-08 13:25:44 +0200

    Added disintegrating blocks.

---
+++ ClawClone/Assets/DisintegratingBlock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisintegratingBlock : MonoBehaviour
+{
+
+    private Animator _animator;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _animator = GetComponent<Animator>();
+	}
+	
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            _animator.SetBool("isDisintegrating", true);
+        }
+    }
+
+    // Used in Disintegrate animation as animation event 
+    private void DestroyBlock()
+    {
+        Destroy(gameObject.transform.parent.gameObject);
+    }
+}
