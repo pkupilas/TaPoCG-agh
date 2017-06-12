e060db12d1967a71c75c1b17cb3a1d31ac4cd99a
aalices <aa.warchal@gmail.com>
2017-06-12 10:17:50 +0200

    Add main menu + fix SkillPanel ref

---
+++ ClawClone/Assets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    [SerializeField]
+    private Button _playButton;
+    [SerializeField]
+    private Button _exitButton;
+
+    void Start () {
+        _playButton = _playButton.GetComponent<Button>();
+        _exitButton = _exitButton.GetComponent<Button>();
+    }
+	
+	public void ExitPress()
+    {
+        Application.Quit();
+    }
+
+    public void PlayPress()
+    {
+        SceneManager.LoadScene("NewMapLevel1");
+    }
+}
